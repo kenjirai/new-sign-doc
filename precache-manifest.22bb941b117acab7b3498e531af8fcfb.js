19aa3d6a6651ee67bd2787209d718b3c5b64ba21
decentralMind <decentralmind@gmail.com>
2019-04-25 09:34:45 +0545

    Updates

---
+++ precache-manifest.22bb941b117acab7b3498e531af8fcfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "427964dd8c68f118a440",
+    "url": "/new-sign-doc/static/js/runtime~main.b26e590f.js"
+  },
+  {
+    "revision": "03fa46a6e432466b4513",
+    "url": "/new-sign-doc/static/js/main.244a4d09.chunk.js"
+  },
+  {
+    "revision": "5ce2780bc7d617fe6b70",
+    "url": "/new-sign-doc/static/js/2.9ad583b1.chunk.js"
+  },
+  {
+    "revision": "03fa46a6e432466b4513",
+    "url": "/new-sign-doc/static/css/main.fb95e005.chunk.css"
+  },
+  {
+    "revision": "5ce2780bc7d617fe6b70",
+    "url": "/new-sign-doc/static/css/2.a246adea.chunk.css"
+  },
+  {
+    "revision": "a65acc4bb92d58e22a919f1c1ec79f9c",
+    "url": "/new-sign-doc/index.html"
+  }
+];
