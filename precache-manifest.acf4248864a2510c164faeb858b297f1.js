51ece69bca9caebfd379161af86c71eaed96c1a2
decentralMind <decentralmind@gmail.com>
2019-04-25 10:45:36 +0545

    Updates

---
+++ precache-manifest.acf4248864a2510c164faeb858b297f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "427964dd8c68f118a440",
+    "url": "/new-sign-doc/static/js/runtime~main.b26e590f.js"
+  },
+  {
+    "revision": "cbcb31916e1056375ab6",
+    "url": "/new-sign-doc/static/js/main.eb55e739.chunk.js"
+  },
+  {
+    "revision": "396a658a5dc3c082baa2",
+    "url": "/new-sign-doc/static/js/2.9bbfdba2.chunk.js"
+  },
+  {
+    "revision": "cbcb31916e1056375ab6",
+    "url": "/new-sign-doc/static/css/main.fb95e005.chunk.css"
+  },
+  {
+    "revision": "396a658a5dc3c082baa2",
+    "url": "/new-sign-doc/static/css/2.a246adea.chunk.css"
+  },
+  {
+    "revision": "53556bb52f7675e80aa332bdb85dc472",
+    "url": "/new-sign-doc/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/new-sign-doc/precache-manifest.fcb388fb2f6ef2fb41231675db7cc06e.js"
+  "/new-sign-doc/precache-manifest.acf4248864a2510c164faeb858b297f1.js"
 );
 
 workbox.clientsClaim();
